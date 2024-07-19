a2582292be1095cf04e7b2fb52feb4b4a3d25fc1
Justin Nguyen <justinchasez04121990@gmail.com>
2024-07-19 09:11:52 +0700

    Added more tests

--- LinQToSqlBuilder.DataAccessLayer.Tests/LinQToSqlDeleteCommandTests.cs
+++ LinQToSqlBuilder.DataAccessLayer.Tests/LinQToSqlDeleteCommandTests.cs
@@ -1,4 +1,5 @@
-﻿using DotNetBrightener.LinQToSqlBuilder;
+﻿using System.Linq.Expressions;
+using DotNetBrightener.LinQToSqlBuilder;
 using LinQToSqlBuilder.DataAccessLayer.Tests.Entities;
 using NUnit.Framework;
 
@@ -10,14 +11,63 @@ public class LinQToSqlDeleteCommandTests
     [Test]
     public void DeleteByFieldValue()
     {
-        var query = SqlBuilder.Delete<CloneUserGroup>(_ => _.IsDeleted);
+        var testCases = new List<TestCase>()
+        {
+            new TestCase()
+            {
+                Expression = g => g.IsDeleted,
+                ExpectedQueryString =
+                    "DELETE FROM [dbo].[CloneUserGroup] WHERE [dbo].[CloneUserGroup].[IsDeleted] = @Param1",
+                ExpectedParamValues = [true]
+            },
+            new TestCase()
+            {
+                Expression          = g => g.Id == 1,
+                ExpectedQueryString = "DELETE FROM [dbo].[CloneUserGroup] WHERE [dbo].[CloneUserGroup].[Id] = @Param1",
+                ExpectedParamValues = [1]
+            },
+            new TestCase()
+            {
+                Expression = g => g.Name == "Test",
+                ExpectedQueryString =
+                    "DELETE FROM [dbo].[CloneUserGroup] WHERE [dbo].[CloneUserGroup].[Name] = @Param1",
+                ExpectedParamValues = ["Test"]
+            },
+            new TestCase()
+            {
+                Expression = g => g.Name == "Test" && g.IsDeleted,
+                ExpectedQueryString =
+                    "DELETE FROM [dbo].[CloneUserGroup] WHERE ([dbo].[CloneUserGroup].[Name] = @Param1 AND [dbo].[CloneUserGroup].[IsDeleted] = @Param2)",
+                ExpectedParamValues = ["Test", true]
+            },
+            new TestCase()
+            {
+                Expression = g => g.Name == "Test" && g.IsDeleted && g.Id == 1,
+                ExpectedQueryString =
+                    "DELETE FROM [dbo].[CloneUserGroup] WHERE (([dbo].[CloneUserGroup].[Name] = @Param1 AND [dbo].[CloneUserGroup].[IsDeleted] = @Param2) AND [dbo].[CloneUserGroup].[Id] = @Param3)",
+                ExpectedParamValues = ["Test", true, 1]
+            }
+        };
 
+        foreach (var testCase in testCases)
+        {
+            var query = SqlBuilder.Delete<CloneUserGroup>(testCase.Expression);
 
-        Assert.That(query.CommandText,
-                    Is.EqualTo(
-                               "DELETE FROM [dbo].[CloneUserGroup] WHERE [dbo].[CloneUserGroup].[IsDeleted] = @Param1"));
+            Assert.That(query.CommandText,
+                        Is.EqualTo(testCase.ExpectedQueryString));
 
-        Assert.That(query.CommandParameters.Values.FirstOrDefault(),
-                    Is.EqualTo(true));
+            for (var i = 0; i < testCase.ExpectedParamValues.Count; i++)
+            {
+                Assert.That(query.CommandParameters.Values.ElementAt(i),
+                            Is.EqualTo(testCase.ExpectedParamValues[i]));
+            }
+        }
     }
+}
+
+class TestCase
+{
+    public Expression<Func<CloneUserGroup, bool>> Expression;
+    public string                                 ExpectedQueryString;
+    public List<object>                           ExpectedParamValues = new List<object>();
 }
